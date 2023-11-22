00fae632e0a8c6d3fa98d325bc8bf70510fb8438
Daniel03 <59920007+WEgeophysics@users.noreply.github.com>
2023-11-22 15:25:29 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -141,7 +141,8 @@ html_theme =  "pydata_sphinx_theme"
 
 
 html_static_path = ['_static', 'example_thumbs']
-html_css_files =  [f"css/custom.css?v={watex.__version__}"]
+# html_css_files =  [f"css/custom.css?v={watex.__version__}"]
+html_css_files =  ['css/custom.css?v=0.3.0']
 
 # todo_include_todos = True
 html_logo = "_static/logo.svg"
